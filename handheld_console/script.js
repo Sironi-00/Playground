7eb9f124aaa45a2fea9c15c41cd0c19af427a288
Sironi-00 <00sironi@gmail.com>
2022-11-11 11:29:21 +0000

    add ids in prep for js logic

--- handheld_console/script.js
+++ handheld_console/script.js
@@ -5,3 +5,26 @@
 // ? dettachable joycons
 
 // switch between R&B and B&W
+
+
+id = `
+id="power"
+id="volume"
+id="ltrigger"
+id="minus"
+id="lstick"
+id="up"
+id="right"
+id="down"
+id="left"
+id="sys-rec"
+id="screen"
+id="rtrigger"
+id="plus"
+id="rstick"
+id="x"
+id="a"
+id="b"
+id="y"
+id="sys-home"
+`
