04b04e99597f5eaeabbdebb2a1be888f4faa9f01
Sironi-00 <00sironi@gmail.com>
2022-11-24 01:07:33 +0000

    maded pixel grid + basic selection logic

--- handheld_console/script.js
+++ handheld_console/script.js
@@ -54,32 +54,62 @@ function main() {
   let b = document.getElementById("b");
   let y = document.getElementById("y");
 
-
   ////////////////////////////////////
   // Screen Power
   let power_state = true;
   let switched = () => {
-      if (power_state) {
-        screen.style.background = "#000";
-        power_state = !power_state;
+    if (power_state) {
+      screen.style.background = "#000";
+      document.documentElement.style.setProperty("--screen", "none");
+      power_state = !power_state;
     } else {
-        screen.style.background = "";
-        power_state = !power_state;
+      screen.style.background = "";
+      document.documentElement.style.setProperty("--screen", "unset");
+      power_state = !power_state;
     }
   };
   power.addEventListener("click", switched);
-  
+
   //////////////////////////////
   // pixels selector
-  let selected = document.getElementsByClassName("selected")
-  let pix_x = (0, 5);
-  let  pix_y = (0, 5);
+  let pix_x = 2;
+  let pix_y = 2;
 
-  let rw = document.getElementsByClassName("row");
-  let r1 = rw[0].document.getElementsByClassName("pixel");
+  let movePixel = (btn) => {
+    if (pix_x <= 0) {
+      pix_x = 4;
+    } else if (pix_x > 4) {
+      pix_x = 0;
+    }else if (pix_y <= 0) {
+      pix_y = 4;
+    }else if (pix_y > 4) {
+      pix_y = 0;
+    } else {
+      switch (btn) {
+        case "up":
+          pix_y--;
+          break;
+        case "right":
+          pix_x++;
+          break;
+        case "down":
+          pix_y++;
+          break;
+        case "left":
+          pix_x--;
+          break;
+      }
+    }
 
+    console.log(pix_x, pix_y)
+    let pix = document.getElementsByClassName(`pixel r${pix_y}`);
+    pix[pix_x].classList.toggle("selected");
+    console.log("*", pix_x, pix_y)
+  };
 
-  let pix = document.getElementsByClassName("pixel");
-  pix[0].classList.toggle("selected");
+  up.addEventListener("click", () => movePixel("up"));
+  right.addEventListener("click", () => movePixel("right"));
+  down.addEventListener("click", () => movePixel("down"));
+  left.addEventListener("click", () => movePixel("left"));
 }
 main();
