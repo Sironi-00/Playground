676b49c883791b48a083b765ecee03f3c43725e8
Sironi-00 <00sironi@gmail.com>
2022-11-29 14:57:02 +0000

    update guide, add vector tracker, ++

--- handheld_console/script.js
+++ handheld_console/script.js
@@ -35,6 +35,20 @@ function main() {
   let s = document.getElementById("s");
   let a = document.getElementById("a");
 
+  // Tracker > Displays msg on screen 
+  let tracker = document.getElementById("tracker");
+  let track = (msg) => {
+    // makes traker visible
+    tracker.style.display = "unset";
+    // then display msg
+    tracker.innerHTML = msg;
+    // then remove tracker after t
+    let rm_tracker = () => {
+      tracker.style.display = "none";
+    } 
+    setTimeout(rm_tracker,3000)
+  }
+
   ////////////////////////////////////
   // Screen Power
   let power_state = true;
@@ -51,7 +65,7 @@ function main() {
       power_state = !power_state;
     }
   };
-  power.addEventListener("click", ()=> power_toggle());
+  power.addEventListener("click", () => power_toggle());
 
   //////////////////////////////
   // pixels selector axis
@@ -96,37 +110,43 @@ function main() {
     pix[pix_x].classList.toggle("selected");
   };
   // Dpad btn to move pixel
-  up.addEventListener("click", ()=> movePixel("up"));
-  right.addEventListener("click", ()=> movePixel("right"));
-  down.addEventListener("click", ()=> movePixel("down"));
-  left.addEventListener("click", ()=> movePixel("left"));
+  up.addEventListener("click", () => movePixel("up"));
+  right.addEventListener("click", () => movePixel("right"));
+  down.addEventListener("click", () => movePixel("down"));
+  left.addEventListener("click", () => movePixel("left"));
 
   /////////////////////////////
   // color switch toggle
   let sc_color_state = true;
   let sc_color_toggle = () => {
-    sc_color_state = sc_color_state? !sc_color_state: !sc_color_state
+    sc_color_state = sc_color_state ? !sc_color_state : !sc_color_state;
+    // pattern tracker
+    let tracking = `color pointer: ${sc_color_state?"Screen + Border": "Selector"}`
+    track(tracking);
   };
   // color changer
   let cur_color = 0;
   let colors = ["#FFF", "#F00", "#0F0", "#00F", "#000"];
-  
+
   let color_pick = (sign) => {
     // changes color of border
     if ((cur_color <= 0) & (sign == "-")) cur_color = colors.length - 1;
     else if ((cur_color >= colors.length - 1) & (sign == "+")) cur_color = 0;
     else sign == "+" ? cur_color++ : cur_color--;
     // switch pointer of color switcher
-    let color_pointer = sc_color_state? "--border-color": "--selector-color"
-    document.documentElement.style.setProperty(color_pointer, colors[cur_color]);
+    let color_pointer = sc_color_state ? "--border-color" : "--selector-color";
+    document.documentElement.style.setProperty(color_pointer,colors[cur_color]);
     // change screen bg with border color change
-    if (color_pointer=="--border-color") document.documentElement.style.setProperty("--screen-bg", colors[cur_color])
+    if (color_pointer == "--border-color") document.documentElement.style.setProperty("--screen-bg",colors[cur_color]);
+    // Color Tracker
+    let tracking = `Color: ${colors[cur_color]} in [${colors}]`
+    track(tracking);
   };
-  a.addEventListener("click", ()=> sc_color_toggle());
+  a.addEventListener("click", () => sc_color_toggle());
 
-  minus.addEventListener("click", ()=> color_pick("-"));
-  plus.addEventListener("click", ()=> color_pick("+"));
-  
+  minus.addEventListener("click", () => color_pick("-"));
+  plus.addEventListener("click", () => color_pick("+"));
+
   /////////////////////////////////////////////////////
   // Border styler
   let cur_style = 0;
@@ -136,94 +156,153 @@ function main() {
     // switch border radius of selector
     if (cur_style >= styles_list.length - 1) cur_style = 0;
     else cur_style++;
-    document.documentElement.style.setProperty("--border-style", styles_list[cur_style])
+    document.documentElement.style.setProperty("--border-style",styles_list[cur_style]);
+    // pattern tracker
+    let tracking = `Border: ${cur_style+1}/${styles_list.length}`
+    track(tracking);
   };
-  w.addEventListener("click", ()=> border_styler())
+  w.addEventListener("click", () => border_styler());
   ///////////////////////////////////
   // Pulse effect
   let pulse_rate = 0;
   let pulse_freq = () => {
     pulse_rate += 1.1;
-    if (pulse_rate < 0) pulse_rate = 2.2
+    if (pulse_rate < 0) pulse_rate = 2.2;
     else if (pulse_rate > 2.2) pulse_rate = 0;
-    document.documentElement.style.setProperty("--pulse-freq", `${pulse_rate}s`);
-  }
-  s.addEventListener("click", ()=> pulse_freq());
+    document.documentElement.style.setProperty(
+      "--pulse-freq",
+      `${pulse_rate}s`
+    );
+  };
+  s.addEventListener("click", () => pulse_freq());
 
   //////////////////////////////////////////////////
-  // Volume 
-  let scaled = false
+  // Volume
+  let scaled = false;
   let scale_hide = () => {
     // switch pointer of color switcher
     scaled = !scaled;
-    let scale_to = scaled ? "scale(0)" : "none"
-    document.documentElement.style.setProperty("--transf", scale_to)
-  }
-  volume.addEventListener("click", ()=> scale_hide())
+    let scale_to = scaled ? "scale(0)" : "none";
+    document.documentElement.style.setProperty("--transf", scale_to);
+  };
+  volume.addEventListener("click", () => scale_hide());
 
   ////////////////////////////////////////////////////
   // pre set patterns
-  let pattern_pos = 0
-  let patterns = [[
-    [0,0],[0,1], [0,2],[0,3], [0,4],
-    [1,0],[1,1], [1,2],[1,3], [1,4],
-    [2,0],[2,1], [2,2],[2,3], [2,4],
-    [3,0],[3,1], [3,2],[3,3], [3,4],
-    [4,0],[4,1], [4,2],[4,3], [4,4]
-  ], [
-    [0,0], [0,2], [0,4],
-    [1,1], [1,3], 
-    [2,0], [2,2], [2,4],
-    [3,1], [3,3], 
-    [4,0], [4,2], [4,4]
-  ], [
-    [0,0], [0,4],
-    [1,1], [1,3], 
-     [2,2],
-    [3,1], [3,3], 
-    [4,0], [4,4]
-  ], [
-    [0,1], [0,2],[0,3],
-    [2,0], [2,2], [2,4],
-    [4,1], [4,2],[4,3]
-  ], [
-    [0,0], [0,4],
-    [1,2],
-    [2,1], [2,3],
-    [3,2],
-    [4,0], [4,4]
-  ]
-]
+  let pattern_pos = 0;
+  let patterns = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [0, 4],
+      [1, 0],
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [2, 0],
+      [2, 1],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+      [3, 0],
+      [3, 1],
+      [3, 2],
+      [3, 3],
+      [3, 4],
+      [4, 0],
+      [4, 1],
+      [4, 2],
+      [4, 3],
+      [4, 4],
+    ],
+    [
+      [0, 0],
+      [0, 2],
+      [0, 4],
+      [1, 1],
+      [1, 3],
+      [2, 0],
+      [2, 2],
+      [2, 4],
+      [3, 1],
+      [3, 3],
+      [4, 0],
+      [4, 2],
+      [4, 4],
+    ],
+    [
+      [0, 0],
+      [0, 4],
+      [1, 1],
+      [1, 3],
+      [2, 2],
+      [3, 1],
+      [3, 3],
+      [4, 0],
+      [4, 4],
+    ],
+    [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [2, 0],
+      [2, 2],
+      [2, 4],
+      [4, 1],
+      [4, 2],
+      [4, 3],
+    ],
+    [
+      [0, 0],
+      [0, 4],
+      [1, 2],
+      [2, 1],
+      [2, 3],
+      [3, 2],
+      [4, 0],
+      [4, 4],
+    ],
+  ];
   let pre_pattern = () => {
-    let co = patterns[pattern_pos]
+    // pattern as an inner array
+    let co = patterns[pattern_pos];
     for (let i = 0; i < co.length; i++) {
-      let point = co[i]
+      let point = co[i];
       let pix = document.getElementsByClassName(`pixel r${point[0]}`);
       pix[point[1]].classList.toggle("selected");
     }
-  }
-  d.addEventListener("click", ()=> pre_pattern())
+    // pattern tracker
+    let tracking = `Pattern: ${pattern_pos+1}/${patterns.length}`
+    track(tracking);
+  };
+  d.addEventListener("click", () => pre_pattern());
   ////////////////////////////////////
   // pattern matcher
   let pattern_shift = (sign) => {
     // changes the index pointing to a pattern
-    sign == "+" ? pattern_pos++:pattern_pos--
-    if (pattern_pos < 0) pattern_pos = patterns.length - 1 
-    else if (pattern_pos >= patterns.length) pattern_pos = 0
-  }
-  l_trigger.addEventListener("click", ()=> pattern_shift("-"));
-  r_trigger.addEventListener("click", ()=> pattern_shift("+"));
+    sign == "+" ? pattern_pos++ : pattern_pos--;
+    if (pattern_pos < 0) pattern_pos = patterns.length - 1;
+    else if (pattern_pos >= patterns.length) pattern_pos = 0;
+  };
+  l_trigger.addEventListener("click", () => pattern_shift("-"));
+  r_trigger.addEventListener("click", () => pattern_shift("+"));
   /////////////////////////////////////////////////////////
   // clear screen
   let clear_patterns = () => {
-    let co = patterns[0]
+    let co = patterns[0];
     for (let i = 0; i < co.length; i++) {
-      let point = co[i]
+      let point = co[i];
       let pix = document.getElementsByClassName(`pixel r${point[0]}`);
       pix[point[1]].classList.remove("selected");
     }
-  }
-  home.addEventListener("click", ()=> clear_patterns())
+  };
+  home.addEventListener("click", () => clear_patterns());
+  ///////////////////////////////////////////
+  // trackers (pattern, color, )
+  
 
   // key events
   nintendo.focus();
@@ -233,8 +312,8 @@ function main() {
       case "1":
         power_toggle();
         break;
-        case "2":
-          scale_hide();
+      case "2":
+        scale_hide();
         break;
       case "ArrowUp":
         e.preventDefault();
@@ -258,29 +337,29 @@ function main() {
       case "e":
         color_pick("+");
         break;
-        case "w":
-          border_styler();
-          break;
-        case "d":
-          pre_pattern();
-          break;
-        case "s":
-          pulse_freq()
-          break;
-        case "a":
-          sc_color_toggle();
-          break;
-        case "z":
-        pattern_shift("-")
+      case "w":
+        border_styler();
+        break;
+      case "d":
+        pre_pattern();
+        break;
+      case "s":
+        pulse_freq();
+        break;
+      case "a":
+        sc_color_toggle();
+        break;
+      case "z":
+        pattern_shift("-");
         break;
       case "x":
         pattern_shift("+");
         break;
-      case "i":
+      case "o":
         clear_patterns();
         break;
-      case "o":
-        
+      case "i":
+        console.log("coming soon");
         break;
       default:
         break;
